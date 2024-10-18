2101eee8dc2a49ad7b7c3aae58e857f08ef4f58d
DaCodingBeast <146308828+DaCodingBeast@users.noreply.github.com>
2024-10-17 19:46:22 -0700

    Jitpack should work Now

---
+++ pso4arms/build/generated/source/buildConfig/release/com/pso4pids/pso4arms/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.pso4pids.pso4arms;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.pso4pids.pso4arms";
+  public static final String BUILD_TYPE = "release";
+}
